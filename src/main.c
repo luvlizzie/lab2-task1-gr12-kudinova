e7601580a0edc158dc8b875edcd6fc8816d5bebe
luvlizzie <kudinovaea2006@yandex.by>
2026-02-26 16:34:34 +0000

    Add array filling and printing functions

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  * Function: is_palindrome
@@ -27,10 +28,40 @@ int is_palindrome(int number) {
 }
 
 /*
- * Main function for testing is_palindrome
+ * Function: fill_array_random
+ * ----------------------------
+ *   array: array to fill
+ *   size: size of the array
+ *   
+ *   fills array with random numbers from 1 to 999
+ */
+void fill_array_random(int array[], int size) {
+		for (int i = 0; i < size; i++) {
+				array[i] = rand() % 998 + 1;  // random numbers from 1 to 999
+		}
+}
+
+/*
+ * Function: print_array
+ * ----------------------
+ *   array: array to print
+ *   size: size of the array
+ *   
+ *   prints array elements in one line
+ */
+void print_array(int array[], int size) {
+		printf("Array: ");
+		for (int i = 0; i < size; i++) {
+				printf("%d ", array[i]);
+		}
+		printf("\n");
+}
+
+/*
+ * Main function for testing fill_array_random and print_array
  */
 int main() {
-		// Тест функции на нескольких числах
+		// Тест is_palindrome
 		int test_numbers[] = {121, 123, 454, 789, 999, 12321};
 		int test_size = 6;
 
@@ -40,6 +71,12 @@ int main() {
 							 test_numbers[i], 
 							 is_palindrome(test_numbers[i]) ? "palindrome" : "not palindrome");
 		}
+
+		// Тест fill_array_random и print_array
+		printf("\nTesting array functions:\n");
+		int array[10];
+		fill_array_random(array, 10);
+		print_array(array, 10);
 
 		return 0;
 }
